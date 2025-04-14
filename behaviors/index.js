2745b3dccc25112b264f88611029f931ba9297e9
c0dezyy <137903769+c0dezyy@users.noreply.github.com>
2025-04-15 01:44:01 +0800

    Create index.js

---
+++ behaviors/index.js
@@ -0,0 +1,25 @@
+const { buryInGround, moveToSafePlace } = require('./nightSafety');
+const { avoidMobs } = require('./mobAvoidance');
+const { preventFall, unstuckPath } = require('./pathing');
+const { findFood } = require('./hunting');
+
+module.exports = {
+  handleNightSafety: (bot) => {
+    moveToSafePlace(bot);
+    buryInGround(bot);
+  },
+  handleHunger: (bot) => {
+    findFood(bot);
+  },
+  handleMobAvoidance: (bot) => {
+    avoidMobs(bot);
+  },
+  handlePathing: (bot) => {
+    preventFall(bot);
+    unstuckPath(bot);
+  },
+  respawnIfDead: (bot) => {
+    bot.queue('respawn', {});
+    console.log('Respawning Noxell...');
+  }
+};
